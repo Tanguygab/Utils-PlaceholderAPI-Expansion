8a98dcfb7cd21ec881ba5358b5aa5551237a9c23
Tanguygab <tanguy@gabril.com>
2023-03-19 15:51:57 +0100

    rebranded as Placeholder-Expansion

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# NestedPlaceholders-PlaceholderAPI-Expansion
-Parse nested PAPI placeholders within PAPI placeholders
+# Parse-Expansion
+Parse colors and nested PAPI placeholders within PAPI placeholders
 
 # Usage
-`%nested_<placeholder>%` - Parses the placeholder once with inner placeholders  
-`%nested_#_<placeholder>%` - Parses the placeholder # times with inner placeholders each time
+`%placeholder_parse_<placeholder>%` - Parses the placeholder once with inner placeholders  
+`%placeholder_parse:#_<placeholder>%` - Parses the placeholder # times with inner placeholders each time
+(Also supports relational placeholders! Just add `rel_` after the first `%`)
 
 # Example
 `%javascript_test%` returns `%player_name%` which returns a player's name  
-`%nested_javascript_test%` or `%nested_2_javascript_test%` will be parsed 2 times, it will first be parsed into %player_name% and then parsed again into the player's name.
+`%placeholder_parse:2_javascript_test%` will be parsed 2 times, it will first be parsed into %player_name% and then parsed again into the player's name.
+
+`%placeholder_parse_server_online_{player_world}%` will parse `{player_world}` first and then `%server_online_` + output of the world placeholder + `%`
+
+Since newer versions, PAPI doesn't parse colors anymore, so `%luckperms_prefix%` returns `&4Admin &c` as plain text.
+`%placeholder_color_luckperms_prefix%` allows you to parse colors, thus returning `§4Admin §c`
---
+++ src/main/java/io/github/tanguygab/placeholderexpansion/PlaceholderExpansion.java
@@ -0,0 +1,126 @@
+package io.github.tanguygab.placeholderexpansion;
+
+
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.Relational;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import java.util.*;
+
+public final class PlaceholderExpansion extends me.clip.placeholderapi.expansion.PlaceholderExpansion implements Relational {
+
+    @Override
+    public List<String> getPlaceholders() {
+        return Arrays.asList("%placeholder_parse_<placeholder>%",
+                "%placeholder_parse:<num>_<placeholder>%",
+                "%placeholder_color_<placeholder>%",
+                "%rel_placeholder_parse_<placeholder>%",
+                "%rel_placeholder_parse:<num>_<placeholder>%",
+                "%rel_placeholder_color_<placeholder>%");
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "placeholder";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Tanguygab";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public String onRequest(OfflinePlayer player, String params) {
+        return process(params,player,null);
+    }
+    @Override
+    public String onPlaceholderRequest(Player viewer, Player target, String params) {
+        return process(params,viewer,target);
+    }
+
+    private String process(String params, OfflinePlayer viewer, Player target) {
+        String output = null;
+        String arg = params.split("_")[0];
+        String text = params.substring(arg.length()+1);
+        if (arg.startsWith("parse")) {
+            int number = 1;
+            if (arg.startsWith("parse:")) {
+                try {number = Integer.parseInt(arg.substring(6));}
+                catch (Exception ignored) {}
+            }
+            output = processParse(text,number,viewer,target);
+        }
+        else if (arg.equalsIgnoreCase("color"))
+            output = ChatColor.translateAlternateColorCodes('&',processParse(text,1,viewer,target));
+        return output;
+    }
+    private String processParse(String text, int number, OfflinePlayer viewer, Player target) {
+        text = "%"+text+"%";
+
+        for (int i = 0; i < number; i++) {
+            findBracketPlaceholders(text);
+            text = parseBracketPlaceholders(text.replace("\\",""),viewer,null);
+            text = parsePlaceholders(text,viewer,target);
+        }
+        return text;
+    }
+
+    private String parsePlaceholders(String text, OfflinePlayer viewer, Player target) {
+        if (target == null)
+            return PlaceholderAPI.setPlaceholders(viewer,text);
+        return PlaceholderAPI.setRelationalPlaceholders(viewer.getPlayer(),target,text);
+    }
+
+    private final Map<String,Map<Integer,Integer>> innerPlaceholders = new HashMap<>();
+    private void findBracketPlaceholders(String params) {
+        if (innerPlaceholders.containsKey(params)) return;
+        char[] chars = params.toCharArray();
+        int newPos = 0;
+        Map<Integer,Integer> innerPlaceholders = new LinkedHashMap<>();
+        List<Integer> brackets = new ArrayList<>();
+        for (int i=0; i < chars.length; i++) {
+            char c = chars[i];
+            boolean escaped = i != 0 && chars[i-1]=='\\';
+            if (escaped) {
+                newPos++;
+                continue;
+            }
+            if (c == '{')
+                brackets.add(i-newPos);
+            if (c == '}' && !brackets.isEmpty()) {
+                innerPlaceholders.put(brackets.get(brackets.size()-1),i-newPos);
+                brackets.remove(brackets.size()-1);
+            }
+        }
+        this.innerPlaceholders.put(params,innerPlaceholders);
+    }
+
+    public String parseBracketPlaceholders(String params, OfflinePlayer viewer, Player target) {
+        Map<Integer,Integer> innerPlaceholders = this.innerPlaceholders.get(params);
+        StringBuilder str = new StringBuilder(params);
+        Map<Integer,Integer> newPositions = new LinkedHashMap<>();
+        for (int pos1 : innerPlaceholders.keySet()) {
+            int pos2 = innerPlaceholders.get(pos1);
+
+            for (int p : newPositions.keySet()) {
+                int l = newPositions.get(p);
+                if (p < pos1) pos1-=l;
+                if (p < pos2) pos2-=l;
+            }
+            String sub = str.substring(pos1,pos2+1);
+            String parsed = parsePlaceholders("%"+sub.substring(1,sub.length()-1)+"%",viewer,target);
+
+            str.replace(pos1, pos2 + 1, parsed);
+
+            newPositions.put(pos1,sub.length()-parsed.length());
+        }
+        return str.toString();
+    }
+}
